669592ebd3d43256337cf303d9e47285676cd2de
Agustin Sartorio <sartorio85@gmail.com>
2022-03-01 19:14:17 -0300

    deploy

--- index.js
+++ index.js
@@ -17,15 +17,15 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
-const getAllTemperaments = require('./controllers/temperaments')
+const getAllTemperaments = require("./controllers/temperaments");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT || 3001, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
     getAllTemperaments();
   });
 });
